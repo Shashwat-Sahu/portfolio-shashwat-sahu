00cb7eb300d343520203e1890f890ddd9d65cee6
Shashwat-Sahu <shashwatsahu25@gmail.com>
2020-10-03 21:07:15 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static('client/portfolio/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','portfolio','build','index.html'))
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port : ",5000);
+})
